1855876aab3f4f2de753fda8a9bb9f686df65fe8
Sam <sharrisdev@gmail.com>
2025-02-24 10:59:37 +0000

    added description to tags

--- tags/models.py
+++ tags/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
